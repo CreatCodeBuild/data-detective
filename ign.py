865c88582393b92524935e1f90de7be3210f24e9
CreatCodeBuild <wangxuanzhealbert@gmail.com>
2017-01-02 22:41:15 -0500

    + ign.html

--- ign.py
+++ ign.py
@@ -1,8 +1,23 @@
 import pandas
-print(pandas.__version__)
+from bokeh.plotting import figure, output_file, show
+
+
 ign = pandas.read_csv('ign.csv')
 
 release_year = ign.get('release_year')
 counts = release_year.value_counts()
 counts.sort_index(inplace=True)
-print(counts)
+
+
+### Bokeh
+# output to static HTML file
+output_file("ign.html")
+
+# create a new plot with a title and axis labels
+p = figure(title="Release Year", x_axis_label='Year', y_axis_label='Quantity')
+
+# add a line renderer with legend and line thickness
+p.line(counts.keys(), counts.get_values(), legend="Temp.", line_width=2)
+
+# show the results
+show(p)
